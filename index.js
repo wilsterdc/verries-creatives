c5bd7b377c186f725e26f3f0d165c3366346738d
wilsterdc <delacruz.wilster15@gmail.com>
2023-08-03 05:19:46 +0800

    nodejs

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-const http = require('http');
+const http = require('http')
 const fs = require('fs')
+const path = require('path')
 const port = 4000;
 
 const server = http.createServer((req, res) => {
-    fs.readFile('./index.html', (err, data) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.write(data);
-        return res.end();
-    })
+    const page = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf-8')
+    const style = fs.readFileSync(path.resolve(__dirname, './style/index.css'), 'utf-8')
+    
+    console.log(req.url)
+    // if (req.url === '/style/index.css') {
+    //     fs.readFileSync(path.join(__dirname, './style/index.css'), (err, data) => {
+    //         res.writeHead(200, {'content-type': 'text/css'})
+    //         res.end(data)
+    //     })
+    // }
+
+    res.writeHead(200, {'content-type': 'text/html'})
+    res.write(style)
+    res.end(page)
 });
 
 server.listen(port, () => {
